86aa84dda35041418f74d687da7cc1fdba835150
Nico <nico.aldag@web.de>
2023-08-02 11:35:01 +0200

    3. Iteration :: Polymorphie

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,11 +4,25 @@ public class App {
     
     public static void main(String[] args) {
      
-        
+        Queen queen = new Queen();
+        Worker worker = new Worker();
+        Drone drone = new Drone();
+
+        pollObject(queen);
+        pollObject(worker);
+        pollObject(drone);
+
     }
 
+    //---- 3. Iteration :: Polymorphie ---//
+
+    private static void pollObject(HoneyBee obj) {
+        output(obj.doYourJob());
+        output(obj.fly());
+        output("----------------------");
+    }
    
-   
+
     private static void output(String outputStr) {
         System.out.println(outputStr);
     }
